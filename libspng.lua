de583f6399788f842a85c39b38650e8fad265784
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2021-12-24 10:47:41 +0200

    unimportant

--- libspng.lua
+++ libspng.lua
@@ -2,7 +2,138 @@
 --libspng LuaJIT binding.
 --Written by Cosmin Apreutesei. Public Domain.
 
+local bit = require'bit'
 local ffi = require'ffi'
 require'libspng_h'
 local C = ffi.load'spng'
 
+local spng = {}
+
+function spng.load(opt)
+
+	local buf, sz = opt.read_buffer, opt.read_buffer_size
+	if not buf then
+		sz = sz or 4096
+		buf = ffi.new('uint8_t[?]', sz)
+	end
+
+	local ctx = C.spng_ctx_new(0)
+	C.spng_set_png_buffer(ctx, buf, sz)
+
+	local fmt = C.SPNG_FMT_RGBA8
+	local flags = bit.bor(
+		C.SPNG_DECODE_TRNS ,
+		C.SPNG_DECODE_GAMMA,
+		C.SPNG_DECODE_PROGRESSIVE
+	)
+	C.spng_decode_image(ctx, nil, 0, fmt, flags)
+
+	local out_size = ffi.new'size_t[1]'
+	C.spng_decoded_image_size(ctx, C.SPNG_FMT_RGBA8, out_size)
+
+	local image_width = image_size / ihdr.height
+	local row_info = ffi.new'struct spng_row_info'
+	while true do
+		local error = C.spng_get_row_info(ctx, row_info)
+		if error ~= 0 break
+		local row = image + image_width * row_info.row_num
+		error = C.spng_decode_row(ctx, row, len)
+	end
+
+	int spng_decode_chunks(spng_ctx *ctx);
+
+	local function free()
+		C.spng_ctx_free(ctx)
+	end
+
+	return {free = free}
+end
+
+typedef int spng_read_fn(spng_ctx *ctx, void *user, void *dest, size_t length);
+typedef int spng_write_fn(spng_ctx *ctx, void *user, void *src, size_t length);
+
+typedef int spng_rw_fn(spng_ctx *ctx, void *user, void *dst_src, size_t length);
+
+spng_ctx *spng_ctx_new(int flags);
+spng_ctx *spng_ctx_new2(struct spng_alloc *alloc, int flags);
+void spng_ctx_free(spng_ctx *ctx);
+
+int spng_set_png_buffer(spng_ctx *ctx, const void *buf, size_t size);
+int spng_set_png_stream(spng_ctx *ctx, spng_rw_fn *rw_func, void *user);
+int spng_set_png_file(spng_ctx *ctx, FILE *file);
+
+void *spng_get_png_buffer(spng_ctx *ctx, size_t *len, int *error);
+
+int spng_set_image_limits(spng_ctx *ctx, uint32_t width, uint32_t height);
+int spng_get_image_limits(spng_ctx *ctx, uint32_t *width, uint32_t *height);
+
+int spng_set_chunk_limits(spng_ctx *ctx, size_t chunk_size, size_t cache_size);
+int spng_get_chunk_limits(spng_ctx *ctx, size_t *chunk_size, size_t *cache_size);
+
+int spng_set_crc_action(spng_ctx *ctx, int critical, int ancillary);
+
+int spng_set_option(spng_ctx *ctx, enum spng_option option, int value);
+int spng_get_option(spng_ctx *ctx, enum spng_option option, int *value);
+
+int spng_decoded_image_size(spng_ctx *ctx, int fmt, size_t *len);
+
+/* Encode/decode */
+int spng_get_row_info(spng_ctx *ctx, struct spng_row_info *row_info);
+
+/* Encode */
+int spng_encode_image(spng_ctx *ctx, const void *img, size_t len, int fmt, int flags);
+
+/* Progressive encode */
+int spng_encode_scanline(spng_ctx *ctx, const void *scanline, size_t len);
+int spng_encode_row(spng_ctx *ctx, const void *row, size_t len);
+int spng_encode_chunks(spng_ctx *ctx);
+
+int spng_get_ihdr(spng_ctx *ctx, struct spng_ihdr *ihdr);
+int spng_get_plte(spng_ctx *ctx, struct spng_plte *plte);
+int spng_get_trns(spng_ctx *ctx, struct spng_trns *trns);
+int spng_get_chrm(spng_ctx *ctx, struct spng_chrm *chrm);
+int spng_get_chrm_int(spng_ctx *ctx, struct spng_chrm_int *chrm_int);
+int spng_get_gama(spng_ctx *ctx, double *gamma);
+int spng_get_gama_int(spng_ctx *ctx, uint32_t *gama_int);
+int spng_get_iccp(spng_ctx *ctx, struct spng_iccp *iccp);
+int spng_get_sbit(spng_ctx *ctx, struct spng_sbit *sbit);
+int spng_get_srgb(spng_ctx *ctx, uint8_t *rendering_intent);
+int spng_get_text(spng_ctx *ctx, struct spng_text *text, uint32_t *n_text);
+int spng_get_bkgd(spng_ctx *ctx, struct spng_bkgd *bkgd);
+int spng_get_hist(spng_ctx *ctx, struct spng_hist *hist);
+int spng_get_phys(spng_ctx *ctx, struct spng_phys *phys);
+int spng_get_splt(spng_ctx *ctx, struct spng_splt *splt, uint32_t *n_splt);
+int spng_get_time(spng_ctx *ctx, struct spng_time *time);
+int spng_get_unknown_chunks(spng_ctx *ctx, struct spng_unknown_chunk *chunks, uint32_t *n_chunks);
+
+/* Official extensions */
+int spng_get_offs(spng_ctx *ctx, struct spng_offs *offs);
+int spng_get_exif(spng_ctx *ctx, struct spng_exif *exif);
+
+
+int spng_set_ihdr(spng_ctx *ctx, struct spng_ihdr *ihdr);
+int spng_set_plte(spng_ctx *ctx, struct spng_plte *plte);
+int spng_set_trns(spng_ctx *ctx, struct spng_trns *trns);
+int spng_set_chrm(spng_ctx *ctx, struct spng_chrm *chrm);
+int spng_set_chrm_int(spng_ctx *ctx, struct spng_chrm_int *chrm_int);
+int spng_set_gama(spng_ctx *ctx, double gamma);
+int spng_set_gama_int(spng_ctx *ctx, uint32_t gamma);
+int spng_set_iccp(spng_ctx *ctx, struct spng_iccp *iccp);
+int spng_set_sbit(spng_ctx *ctx, struct spng_sbit *sbit);
+int spng_set_srgb(spng_ctx *ctx, uint8_t rendering_intent);
+int spng_set_text(spng_ctx *ctx, struct spng_text *text, uint32_t n_text);
+int spng_set_bkgd(spng_ctx *ctx, struct spng_bkgd *bkgd);
+int spng_set_hist(spng_ctx *ctx, struct spng_hist *hist);
+int spng_set_phys(spng_ctx *ctx, struct spng_phys *phys);
+int spng_set_splt(spng_ctx *ctx, struct spng_splt *splt, uint32_t n_splt);
+int spng_set_time(spng_ctx *ctx, struct spng_time *time);
+int spng_set_unknown_chunks(spng_ctx *ctx, struct spng_unknown_chunk *chunks, uint32_t n_chunks);
+
+/* Official extensions */
+int spng_set_offs(spng_ctx *ctx, struct spng_offs *offs);
+int spng_set_exif(spng_ctx *ctx, struct spng_exif *exif);
+
+const char *spng_strerror(int err);
+const char *spng_version_string(void);
+
+return spng
--- libspng.md
+++ libspng.md
@@ -7,6 +7,89 @@ A ffi binding of [libspng](https://libspng.org/).
 
 <warn>Work in progress</warn>
 
------------------------------------------ ------------------------------------
-WIP
------------------------------------------ ------------------------------------
+------------------------------------ -----------------------------------------
+`spng.open(opt | read) -> img`       open a PNG image for decoding
+`img:load([opt]) -> bmp`             load the image into a bitmap
+`img:free()`                         free the image
+`spng.save(opt)`                     compress a bitmap into a PNG image
+------------------------------------ -----------------------------------------
+
+### `spng.open(opt | read) -> img`
+
+Open a PNG image and read its header. `opt` is a table containing at least
+the read function and possibly other options.
+
+The read function has the form `read(buf, size) -> readsize`, it can yield
+and it must signal I/O errors by raising an error. It must accept `nil`
+for `buf` which means skip bytes (i.e. seek). It will only be asked to read
+a positive number of bytes and it can return less bytes than asked,
+including zero which signals EOF.
+
+The `opt` table has the fields:
+
+  * `read`: the read function (required
+  * `read_buffer`: optional, the read buffer to use.
+  * `read_buffer_size`: the read buffer size.
+
+* where to read the data from (one of the following):
+	* `path`: read data from a file given its filename
+	* `string`: read data from a string
+	* `cdata`, `size`: read data from a buffer of specified size
+	* `stream`: read data from an opened `FILE *` stream
+	* `read`: read data from a reader function of form:
+		* `read(needed_size) -> cdata, size | string | nil`
+			* `needed_size` is informative, the function can return however
+			many bytes it wants, as long as it returns at least 1 byte.
+* loading options:
+	* `accept`: if present, it is a table specifying conversion options.
+	  libpng implements many of the pixel conversions itself, while other
+	  conversions are supported through [bmpconv bmpconv.convert_best()].
+	  If no `accept` option is given, the image is returned in a normalize
+	  8 bit per channel, top down, palette expanded, 'g', 'rgb', 'rgba' or
+	  'ga' format.
+	* `[pixel_format] = true` - specify one or more accepted pixel formats
+	  (they are all implicitly 8 bit per channel since that is the only
+	  supported bit depth):
+		* 'rgb', 'bgr', 'rgba', 'bgra', 'argb', 'abgr', 'g', 'ga', 'ag'.
+			* if no pixel format is specified, resulted bit depth will not
+			  necessarily be 8 since no conversion will take place.
+	* `[orientation] = true` - specify one or more accepted orientations:
+		* 'top_down', 'bottom_up' (default is 'top_down')
+	* `padded`: true/false (default is false) - specify that the row stride
+	  should be a multiple of 4
+	* `header_only`: do not decompress the image; return only the image header fields.
+	* `sparkle`: true/false (default is false) - alternative render mode
+	  for interlaced images.
+* callbacks:
+	* `warning`: a function to be called as `warning(msg)` on non-fatal errors.
+	* `render_scan`: a function to be called as `render_scan(image,
+	  is_last_scan, scan_number)` for each pass of an interlaced PNG. It can
+	  be used to implement progressive display of images.
+		* also called once for non-interlaced images.
+		* also called on error, as `render_scan(nil, true, scan_number, error)`,
+		  where `scan_number` is the scan number that was supposed to be
+		  rendering next and `error` the error message.
+
+For more info on decoding process and options, read the [libpng doc]
+(have coffee/ibuprofen ready).
+
+The returned image object is a table with the fields:
+
+* `pixel`, `orientation`, `stride`, `data`, `size`, `w`, `h`: image format
+  and dimensions and pixel data.
+* `file.pixel`, `file.paletted`, `file.bit_depth`, `file.interlaced`,
+  `file.w`, `file.h`: format of the original image before conversion.
+
+## Help needed
+
+  * saving API
+  * jit is turned off because we can't call error() from a ffi callback called
+    from C; and yet we must not return control to C on errors.
+	 Is there a way around it?
+  * the read callback cannot yield since it is called from C code. This means
+    coroutine-based socket schedulers are out, so much for progressive loading.
+	 Is there a way around it?
+
+
+[libpng lib]:  http://www.libpng.org/pub/png/libpng.html
+[libpng doc]:  http://www.libpng.org/pub/png/libpng-1.2.5-manual.html
